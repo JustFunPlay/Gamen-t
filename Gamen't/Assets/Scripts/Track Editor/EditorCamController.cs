6c86677d5c80ab41c3cf95cea4f95b43f79ca2b7
JustFunPlay <78020014+JustFunPlay@users.noreply.github.com>
2022-02-15 13:53:56 +0100

    Track editor camera + scene

---
+++ Gamen't/Assets/Scripts/Track Editor/EditorCamController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EditorCamController : MonoBehaviour
+{
+    public Vector2 moveVector;
+    public Vector2 turnVector;
+
+    public float moveSpeed;
+    public float turnSpeed;
+    public float heightSpeed;
+
+    void OnMoveCam(InputValue moveValue)
+    {
+        moveVector = moveValue.Get<Vector2>();
+    }
+    void OnTurnCam(InputValue turnValue)
+    {
+        turnVector = turnValue.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(0, turnVector.x * turnSpeed, 0);
+        transform.Translate(moveVector.x * moveSpeed, turnVector.y * heightSpeed, moveVector.y * moveSpeed);
+    }
+}
