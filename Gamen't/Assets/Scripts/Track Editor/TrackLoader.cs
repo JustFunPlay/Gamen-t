5d7fc8ce8c911676c5bbd26e78cb39169359c06d
JustFunPlay <78020014+JustFunPlay@users.noreply.github.com>
2022-02-28 11:00:44 +0100

    yeety delety blocks

--- Gamen't/Assets/Scripts/Track Editor/TrackLoader.cs
+++ Gamen't/Assets/Scripts/Track Editor/TrackLoader.cs
@@ -7,9 +7,16 @@ public class TrackLoader : MonoBehaviour
     public TrackToLoad trackToLoad;
     void Start()
     {
-        for (int i = 0; i < trackToLoad.track.trackPieces.Count; i++)
+        NodeInfo[] nodeInfos = FindObjectsOfType<NodeInfo>();
+        foreach  (NodeInfo node in nodeInfos)
         {
-            Instantiate(trackToLoad.track.trackPieces[i].trackPiece, trackToLoad.track.trackPieces[i].position, trackToLoad.track.trackPieces[i].rotation);
+            for (int i = 0; i < trackToLoad.track.trackPieces.Count; i++)
+            {
+                if (trackToLoad.track.trackPieces[i].gridlocation == node.gridNodeValue)
+                {
+                    node.trackPiece = Instantiate(trackToLoad.track.trackPieces[i].trackPiece, trackToLoad.track.trackPieces[i].position, trackToLoad.track.trackPieces[i].rotation);
+                }
+            }
         }
     }
 }
