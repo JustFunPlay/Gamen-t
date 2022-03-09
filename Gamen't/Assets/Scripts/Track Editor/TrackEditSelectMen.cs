cc45f4b1922444c7fef1822f8d7c46eac19c1170
Beau00 <70371863+Beau00@users.noreply.github.com>
2022-03-09 15:58:56 +0100

    aaaaa

--- Gamen't/Assets/Scripts/Track Editor/TrackEditSelectMen.cs
+++ Gamen't/Assets/Scripts/Track Editor/TrackEditSelectMen.cs
@@ -3,22 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
-public class TrackEditSelectMen : MonoBehaviour
+using UnityEngine.EventSystems;
+public class TrackEditSelectMen : MonoBehaviour, ISelectHandler, IDeselectHandler
 {
     public TrackToLoad trackToLoad;
     public TrackInfo trackInfo;
     public ListOfAllTracks allTracks;
+    public int ah;
 
     public Text description;
     public Text trackType;
     public GameObject editBtn;
+    public GameObject infoTrack;
+
 
     public Transform[] transforms;
     public GameObject buttonPrefab;
 
+
     void Start()
     {
+
         UpdateTrackList();
     }
 
@@ -29,9 +34,11 @@ public class TrackEditSelectMen : MonoBehaviour
         //SceneManager.LoadScene(1);
     }
 
-    public void LoadBaseTrack(int track)
+    public void LoadBaseTrack()
     {
-        trackToLoad.track = allTracks.defaultTracks[track];
+        
+
+        trackToLoad.track = allTracks.defaultTracks[ah];
         editBtn.SetActive(false);
         description.text = trackToLoad.track.description;
         if (trackToLoad.track.trackType == TrackType.Circuit)
@@ -43,6 +50,7 @@ public class TrackEditSelectMen : MonoBehaviour
             trackType.text = "Linear";
         }
     }
+
     public void LoadCustomTrack(int track)
     {
         trackToLoad.track = allTracks.trackInfos[track];
@@ -76,5 +84,25 @@ public class TrackEditSelectMen : MonoBehaviour
                 newButton.GetComponent<TrackSelectButton>().index = i;
             }
         }
+    }
+    public void OnSelect(BaseEventData eventData)
+    {
+
+        infoTrack.SetActive(true);
+        if (infoTrack == true)
+        {
+            LoadBaseTrack();
+        }
+
+    }
+
+
+
+
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+
+        infoTrack.SetActive(false);
     }
 }
