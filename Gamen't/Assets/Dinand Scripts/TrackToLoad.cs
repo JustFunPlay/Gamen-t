8822753e682979226b904a0dc1c662aa73801a6d
Beau00 <70371863+Beau00@users.noreply.github.com>
2022-03-14 20:47:54 +0100

    i am fucking done now fuck you allllllllllllllllllllllllllllllllllllllllllll

--- Gamen't/Assets/Dinand Scripts/TrackToLoad.cs
+++ Gamen't/Assets/Dinand Scripts/TrackToLoad.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Track To Load", menuName = "ScriptableObjects/Track To Load")]
+
 public class TrackToLoad : ScriptableObject
 {
     public TrackInfo track;
