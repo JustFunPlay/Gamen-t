0301000da0025ce2367cab40e9d1c000286bb833
Beau00 <70371863+Beau00@users.noreply.github.com>
2022-04-04 10:53:53 +0200

    Revert "Revert "Merge branch 'Track-editor' into Beau-focky-with-phycics""
    This reverts commit defc7f72aa6dfa58c74c94c196259996faf9f6f9.

--- Gamen't/Assets/Dinand Scripts/Track Editor/UpdateTrackInformation.cs
+++ Gamen't/Assets/Dinand Scripts/Track Editor/UpdateTrackInformation.cs
@@ -9,8 +9,8 @@ public class UpdateTrackInformation : MonoBehaviour
     public Text lapText;
     public GameObject addLapBtn;
     public GameObject rmvLapBtn;
-    public Text trackNameText;
-    public Text descriptionText;
+    public InputField trackNameText;
+    public InputField descriptionText;
     public Text trackTypeText;
     public static bool isTested;
 
