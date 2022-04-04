e7012f530f4cab7b84860610cbc4140fd621b4f2
Beau00 <70371863+Beau00@users.noreply.github.com>
2022-04-04 10:52:30 +0200

    Revert "betterder"
    This reverts commit 1de68316ef5a9b30d83a560ab240eff80469412c.

--- Gamen't/Assets/Beau/Script/soundtest.cs
+++ Gamen't/Assets/Beau/Script/soundtest.cs
@@ -9,13 +9,13 @@ public class SoundTest : MonoBehaviour
     public float minPitch;
     public float pitch;
     public NewCarControll newCarControll;
-    
+
 
 
 
     public void Update()
     {
-        pitch = newCarControll.speedRead/150;
+        pitch = newCarControll.speedRead/140;
 
 
         if (pitch < minPitch)
@@ -31,10 +31,6 @@ public class SoundTest : MonoBehaviour
             brrSounds.pitch = pitch;
         }
         
-        if(newCarControll.inputGasBrake.y < 0)
-        {
-            pitch = newCarControll.speedRead;
-        }
 
         
     }
