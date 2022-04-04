b855143125d2055f9a6c814e1e864385dd8bd83e
Beau00 <70371863+Beau00@users.noreply.github.com>
2022-04-04 23:07:35 +0200

    aaaaa

---
+++ Gamen't/Assets/Beau/Script/asdas.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class asdas : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+
+{
+    public Text a;
+    public Color b;
+    public Color c;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        a.color = b;
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        a.color = c;
+    }
+}
