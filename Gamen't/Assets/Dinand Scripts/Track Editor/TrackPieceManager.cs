9b0cbb96659371006e0bd7babc5f675adb69908d
Beau00 <70371863+Beau00@users.noreply.github.com>
2022-04-04 10:52:12 +0200

    Revert "Merge branch 'Track-editor' into Beau-focky-with-phycics"
    This reverts commit dcc69230df283635714dc123574712fe1cd8588a, reversing
    changes made to 1de68316ef5a9b30d83a560ab240eff80469412c.

--- Gamen't/Assets/Dinand Scripts/Track Editor/TrackPieceManager.cs
+++ Gamen't/Assets/Dinand Scripts/Track Editor/TrackPieceManager.cs
@@ -5,26 +5,25 @@ using UnityEngine.InputSystem;
 
 public class TrackPieceManager : EditorCamController
 {
-    public int selectedTrackPieceIndex;
-    public TrackPieceHolder pieceHolder;
+    public GameObject selectedTrackPiece;
     public TrackToLoad toLoad;
-    public UpdateTrackInformation updateTrackInformation;
     void OnPlaceTrack()
     {
-        if (currentNode.trackPiece)
+        if (selectedTrackPiece)
         {
-            OnRemoveTrack();
+            if (currentNode.trackPiece)
+            {
+                OnRemoveTrack();
+            }
+            currentNode.trackPiece = Instantiate(selectedTrackPiece, currentNode.transform.position, Quaternion.identity);
+            TrackPiece newTrackPiece = new TrackPiece();
+            newTrackPiece.gridlocation = currentNode.gridNodeValue;
+            newTrackPiece.trackPiece = selectedTrackPiece;
+            newTrackPiece.rotation = currentNode.trackPiece.transform.rotation;
+            newTrackPiece.position = currentNode.trackPiece.transform.position;
+            newTrackPiece.xScale = 1;
+            toLoad.track.trackPieces.Add(newTrackPiece);
         }
-        currentNode.trackPiece = Instantiate(pieceHolder.trackPieces[selectedTrackPieceIndex], currentNode.transform.position, Quaternion.identity);
-        TrackPiece newTrackPiece = new TrackPiece();
-        newTrackPiece.gridlocation = currentNode.gridNodeValue;
-        newTrackPiece.trackPieceIndex = selectedTrackPieceIndex;
-        newTrackPiece.rotation = currentNode.trackPiece.transform.rotation;
-        newTrackPiece.position = currentNode.trackPiece.transform.position;
-        newTrackPiece.xScale = 1;
-        toLoad.track.trackPieces.Add(newTrackPiece);
-        updateTrackInformation.UpdateInfo();
-        UpdateTrackInformation.isTested = false;
     }
     void OnRemoveTrack()
     {
@@ -39,8 +38,6 @@ public class TrackPieceManager : EditorCamController
         {
             Destroy(currentNode.trackPiece);
         }
-        updateTrackInformation.UpdateInfo();
-        UpdateTrackInformation.isTested = false;
     }
     void OnRotateTrackRight()
     {
@@ -55,7 +52,6 @@ public class TrackPieceManager : EditorCamController
                 }
             }
         }
-        UpdateTrackInformation.isTested = false;
     }
     void OnRotateTrackLeft()
     {
@@ -70,7 +66,6 @@ public class TrackPieceManager : EditorCamController
                 }
             }
         }
-        UpdateTrackInformation.isTested = false;
     }
     void OnFlipTrack()
     {
@@ -87,6 +82,5 @@ public class TrackPieceManager : EditorCamController
                 }
             }
         }
-        UpdateTrackInformation.isTested = false;
     }
 }
