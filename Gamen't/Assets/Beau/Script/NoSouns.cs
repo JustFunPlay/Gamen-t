1b01478fde5b5cefdf75e6356f6cffe1060f81ab
Beau00 <70371863+Beau00@users.noreply.github.com>
2022-04-06 22:05:41 +0200

    Ur Mom

---
+++ Gamen't/Assets/Beau/Script/NoSouns.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoSouns : MonoBehaviour
+{
+
+    private void Update()
+    {
+        GetComponentInChildren<Sound>().brrSounds.Stop();
+
+    }
+
+
+}
