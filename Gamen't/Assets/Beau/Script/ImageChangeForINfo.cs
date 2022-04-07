e814c28ec0cc7fa52f5a7d8358dee8e4c2f0d940
Beau00 <70371863+Beau00@users.noreply.github.com>
2022-04-07 16:17:15 +0200

    aaaaaaaaaaah

--- Gamen't/Assets/Beau/Script/ImageChangeForINfo.cs
+++ Gamen't/Assets/Beau/Script/ImageChangeForINfo.cs
@@ -7,11 +7,27 @@ public class ImageChangeForINfo : MonoBehaviour
 {
     public Sprite[] premadeImages;
     public GameObject trackInfoScreen;
+    public Sprite stockImage;
+    public bool returnStock;
 
     public void PressedButton(int i)
     {
-        trackInfoScreen.GetComponentInChildren<Image>().sprite = premadeImages[i];
+
+
+            trackInfoScreen.GetComponentInChildren<Image>().sprite = premadeImages[i];
+            returnStock = true;
+            A();
+
     }
+    public void A()
+    {
+
+        if (returnStock == true)
+        {
+            trackInfoScreen.GetComponentInChildren<Image>().sprite = stockImage;
+        }
+    }
+    
 
 
 }
